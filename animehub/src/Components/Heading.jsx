971d6deb432d91265f9bbf3afe0f47d95d4281f5
himanshukandari14 <himanshukandari14@gmail.com>
2024-02-11 14:53:32 +0530

    commit

--- animehub/src/Components/Heading.jsx
+++ animehub/src/Components/Heading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Heading = () => {
   return (
     <div className='w-full bg-gray-200 h-[60px] flex justify-center items-center'>
-    <h1 className='text-3xl '>AnimeHub</h1>
+    <h1 className='text-3xl '>AnimeHubb</h1>
       
     </div>
   )
